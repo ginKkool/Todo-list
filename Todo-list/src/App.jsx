a63c5f6130e8742c24f7bfd43f7599312ca54890
ginKkool <ginkko71@gmail.com>
2025-06-10 17:45:33 +0200

    css to do

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -3,7 +3,29 @@ import { TfiMenuAlt } from "react-icons/tfi";
 import { useState } from "react";
 
 function App() {
-  const [event, setElement] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [element, setElement] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTab = [...tasks];
+    newTab.push({
+      title: element,
+      isDone: false,
+    });
+    setTasks(newTab);
+    setElement("");
+  };
+  const handleCheck = (index) => {
+    const newTab = [...tasks];
+    newTab[index].isDone = !newTab[index].isDone;
+    setTasks(newTab);
+  };
+
+  const handleDelete = (index) => {
+    const newTab = [...tasks];
+    newTab.splice(index, 1);
+    setTasks(newTab);
+  };
 
   return (
     <>
@@ -15,17 +37,40 @@ function App() {
       </header>
       <main>
         <div className="inputs">
-          <input
-            type="text"
-            placeholder="new task"
-            onChange={(event) => setElement(event.target.value)}
-          />
-          <button
-            type="text"
-            placeholder="new task"
-            value={element}
-            onChange={(event) => setElement(event.target.value)}
-          />
+          {tasks.map((task, index) => {
+            return (
+              <div className="tasks">
+                <input
+                  checked={task.isDone}
+                  type="checkbox"
+                  onChange={() => {
+                    handleCheck(index);
+                  }}
+                />
+                <span className={task.isDone ? "done-task" : null}>
+                  {task.title}
+                </span>
+                <button
+                  onClick={() => {
+                    handleDelete(index);
+                  }}
+                >
+                  delete
+                </button>
+              </div>
+            );
+          })}
+          <form onSubmit={handleSubmit}>
+            <input
+              value={element}
+              type="text"
+              placeholder="new task"
+              onChange={(event) => {
+                setElement(event.target.value);
+              }}
+            />
+            <button type="submit">add task</button>
+          </form>
         </div>
       </main>
     </>
